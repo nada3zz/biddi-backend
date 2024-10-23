6d65785ac48e22ada299e0e06fb743a1034a9034
nadaeezzdin <nadaeezzdin@gmail.com>
2024-10-23 11:18:16 +0300

    add product schema

--- src/app/modules/product/models/product.entity.ts
+++ src/app/modules/product/models/product.entity.ts
@@ -12,8 +12,8 @@ export class Product {
   @Prop({ type: String, required: true, unique: true, trim: true })
   slug: string;
 
-  @Prop({ type: Array})
-  images: Types.ObjectId[];
+  @Prop({ type: [String], required: true})
+  images: string[];
 
   @Prop({ type: String, required: true })
   description: string;
@@ -24,35 +24,6 @@ export class Product {
   @Prop({ type: Number, required: true })
   price: number;
 
-  @Prop({ type: String, required: true })
-  currency: string;
-
-  @Prop({ type: Number, default: 0 })
-  priceDiscount: number;
-
-  @Prop({ type: Number, required: true })
-  quantity: number;
-
-  @Prop({ type: Number, default: 0 })
-  sold: number;
-
-  @Prop({ type: Boolean, default: false })
-  isOutOfStock: boolean;
-
-  @Prop({
-    type: Number,
-    default: 0,
-    min: 1,
-    max: 5,
-    set: (val: number) => Math.round(val * 10) / 10,
-  })
-  ratingsAverage: number;
-
-  @Prop({ type: Number, default: 0 })
-  ratingsQuantity: number;
-
-  @Prop({ type: Object, required: true })
-  details: Record<string, any>;
 }
 
 type ProductDocument = Product & Document;
